a292780d3e5347fa698595014635323dc86d824d
Dmitry Zobnin <dmit25@hotmail.com>
2016-07-15 12:04:25 +0300

    Replace by regex

---
+++ DZ.Linqpad.Utils/RegexExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+
+namespace DZ.Linqpad.Utils
+{
+    public static class RegexExtensions
+    {
+        public static string ReplaceByRegex(this string text, [RegexPattern] string pattern, string replacement)
+        {
+            return Regex.Replace(text, pattern, replacement);
+        }
+    }
+}
