6bd6a8756e34d11008d46c2617fb6879a746b4be
Dmitry Zobnin <dmit25@hotmail.com>
2016-08-09 16:06:54 +0300

    Some tests added

---
+++ DZ.Linqpad.Utils.Tests/ReplaceByRegexTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DZ.Linqpad.Utils;
+
+namespace DZ.Linqpad.Utils.Tests
+{
+    [TestFixture]
+    public class ReplaceByRegexTests
+    {
+        [Test]
+        public void CanReplaceWithGroupNumber()
+        {
+            Assert.That(
+                "ok lets <test 123> it quickly $34"
+                .ReplaceByRegex(@"<(\w+) (\d+)>", "$2$1")
+                .ReplaceByRegex(@"\$(?<test>\d+)", "${test}"),
+                Is.EqualTo("ok lets 123test it quickly 34"));
+        }
+
+    }
+}
